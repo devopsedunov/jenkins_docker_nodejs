0a65d68c910773101fc33e9e1e03aeea27e97993
swmdevopsedu <44762127+swmdevopsedu@users.noreply.github.com>
2018-12-01 20:14:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n");
+  response.end("Hello jenkins_docker_nodejs\n");
 });
 
-// listen on localhost:8000
+// listen on http://ec2-18-191-209-105.us-east-2.compute.amazonaws.com:8000
 server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+console.log("Server listening at http://ec2-18-191-209-105.us-east-2.compute.amazonaws.com:8000/");
